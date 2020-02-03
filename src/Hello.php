d7df570e6bcc2c6a4b71f613e7816d2f824e7211
thangnq37 <nguyenquocthang3794@gmail.com>
2020-02-03 11:52:30 +0700

    update code

---
+++ src/Hello.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+ * (c) YOUR NAME <nguyenquocthang.dev@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace thangnq37\phpLibrary;
+
+class Hello
+{
+    public function say($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+}
